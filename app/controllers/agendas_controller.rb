22035740db60d850f12e7ffb87d07c9d4dd1d09f
”taro <groveleffect@gmail.com>
2019-12-01 10:16:00 +0900

    アジェンダの削除機能を実装

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -1,5 +1,5 @@
 class AgendasController < ApplicationController
-  # before_action :set_agenda, only: %i[show edit update destroy]
+  before_action :set_agenda, only: %i[show edit update destroy]
 
   def index
     @agendas = Agenda.all
@@ -15,9 +15,22 @@ class AgendasController < ApplicationController
     @agenda.team = Team.friendly.find(params[:team_id])
     current_user.keep_team_id = @agenda.team.id
     if current_user.save && @agenda.save
-      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda') 
+      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda')
     else
       render :new
+    end
+  end
+
+  def destroy
+    if current_user.id == @agenda.team.owner_id || current_user.id == @agenda.user_id
+      @agenda.destroy
+      members = @agenda.team.members
+      members.each do | member |
+        AgendaMailer.delete_mail(@agenda.title, member.email).deliver
+      end
+      redirect_to dashboard_url, notice: 'アジェンダを削除しました'
+    else
+      redirect_to dashboard_url, notice: 'チームオーナーもしくはアジェンダ作成者のみ削除出来ます'
     end
   end
 
